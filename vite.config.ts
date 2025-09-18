29495c1c1cb375815275208978975ec05d21f6fa
patnala sri krishna sai <96357392+krishkrishna03@users.noreply.github.com>
2025-09-19 01:42:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-<<<<<<< HEAD
-// vite.config.ts
-=======
->>>>>>> a655124e77d653c08f2b61172894d4d4f8b7064c
+// https://vitejs.dev/config
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-<<<<<<< HEAD
     proxy: {
-      "/api": "http://localhost:5000",  // forward API requests to backend
+      "/api": "http://localhost:5000", // forward API requests to backend
     },
   },
   plugins: [
     react(),
     mode === "development" && componentTagger(),
-=======
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
->>>>>>> a655124e77d653c08f2b61172894d4d4f8b7064c
-  ].filter(Boolean),
+  ].filter(Boolean), // filter out false in production
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
