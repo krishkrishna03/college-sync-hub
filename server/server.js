ea2ea8fd94406c1de9ae144fdf484173849f2417
patnala sri krishna sai <96357392+krishkrishna03@users.noreply.github.com>
2025-10-06 13:14:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(limiter);
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://eduplant.netlify.app',
+  'https://college-sync-hub-y9n7.vercel.app',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
